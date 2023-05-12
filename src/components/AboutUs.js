512d54d4df2ae64d49cce79ab78221281c0ede16
antsuhue <anderson_julio_15@hotmail.com>
2023-05-11 22:17:50 -0300

    Alteração de layout de cards para grid

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,30 +1,26 @@
-import "../css/AboutUs.css"
-import about from "../static/aboutUs.json"
+import "../css/AboutUs.css";
+import about from "../static/aboutUs.json";
 
 function aboutUs() {
+  const keys = Object.keys(about["about"]);
+  let listCards = [];
 
-    const keys = Object.keys(about["about"])
-    let listCards = []
+  keys.forEach((e) => {
+    listCards.push(
+      <div key={e} className="box">
+        <p>{about["about"][e]}</p>
+      </div>
+    );
+  });
 
-    keys.forEach((e) => {
-        listCards.push(
-            <div key={e} className="box">
-                <p >{about["about"][e]}</p>
-            </div>
-        )
-    })
-
-    return (
-
-        <div className="sessao" id="aboutUs">
-            <div className="title">
-                <h1>Sobre Nós</h1>
-            </div>
-            <div className="content">
-                {listCards}
-            </div>
-        </div>
-    )
+  return (
+    <div className="sessao" id="aboutUs">
+      <div className="title">
+        <h1>Sobre Nós</h1>
+      </div>
+      <div className="content">{listCards}</div>
+    </div>
+  );
 }
 
-export default aboutUs
+export default aboutUs;
